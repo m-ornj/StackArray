bcc62009c6a4638eba8dafc56c2244f544fa212d
Aleksandr Kraev <kraev.aleks@gmail.com>
2025-04-09 17:16:24 -0400

    Make predefined buffer aliases public

--- Sources/StackArray/Buffers.swift
+++ Sources/StackArray/Buffers.swift
@@ -1,31 +1,31 @@
-typealias Buffer64Byte = (
+public typealias Buffer64Byte = (
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64
 )
 
-typealias Buffer128Byte = (
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64
-)
-
-typealias Buffer256Byte = (
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+public typealias Buffer128Byte = (
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64
 )
 
-typealias Buffer512Byte = (
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
-    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+public typealias Buffer256Byte = (
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64
 )
 
-typealias Buffer1024Byte = (
+public typealias Buffer512Byte = (
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
+    UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64
+)
+
+public typealias Buffer1024Byte = (
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
     UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64, UInt64,
